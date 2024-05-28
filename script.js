3f3423e67f330cec6946134e87430426b7178e7e
VictorHxHugo <victorzk6@hotmail.com>
2024-05-28 03:02:12 -0300

    Top-Level Await (ES2022)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 ///////////////////////////////////////
+/*
 // Exporting and Importing in ES6 Modules
 
 // Importing module
@@ -22,4 +23,29 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
-/*
+*/
+
+///////////////////////////////////////
+// Top-Level Await (ES2022)
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
